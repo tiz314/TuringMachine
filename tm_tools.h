5d53d7b008106593e42580ee83d91934af2f760f
tiz314 <63679072+tiz314@users.noreply.github.com>
2022-11-27 18:09:53 +0100

    changes

--- tm_tools.h
+++ tm_tools.h
@@ -1,23 +1,6 @@
 #include "consts.h"
+#include "tm.h"
 #include <string.h>
-
-    /*
-    The instruction schema used for instructions depends on the two main modes of operation of the machine: one or two tapes:
-
-    One tape version:       | STATUS | INPUT | NEXT STATUS | OUTPUT | HEAD MOVEMENT |
-    Two tapes version:      | STATUS | INPUT TAPE 1 | INPUT TAPE 2 | NEXT STATUS | OUTPUT TAPE 1 | OUTPUT TAPE 2 | HEAD 1 MOVEMENT | HEAD 2 MOVEMENT |
-
-    */
-    typedef struct
-{
-    __uint8_t m_mode;                  // Machine mode of operation: it can be 0 (one tape machine) or 1 (two tapes machine)
-    char status;                       // current machine status, starts from 0
-    unsigned short int instructions_n; // number of received instructions
-    unsigned short int tape_position;  // tape position = position in "input" array
-    unsigned short int pos;            // positive counter for the position in the instruction matrix
-    char input[BUFSIZ];                // TM tape. Not dinamically allocated because of the possibility to write new slots by the program itself
-    char backup_input[BUFSIZ];         // backup char array. The original tape is copied also here, in order to recover it without realoding from file.
-} tm_components;
 
 void init_tape(char *tape)
 {
